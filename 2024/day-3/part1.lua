7b049f436ec42a1fbd78cedff439ae0867048ff3
Luke Bergen <lukeabergen@gmail.com>
2024-12-03 20:52:26 -0500

    day 3 part 2

--- 2024/day-3/part1.lua
+++ 2024/day-3/part1.lua
@@ -1,13 +1,15 @@
 local U = require("util")
 
 local mem = U.readFile("./input.txt")
-local sum = 0
 
-for x,y in mem:gmatch("mul%((%d+),(%d+)%)") do
-  print(x * y)
-  sum = sum + (x * y)
+local function sumMuls(str)
+  local sum = 0
+  for x,y in mem:gmatch("mul%((%d+),(%d+)%)") do
+    sum = sum + (x * y)
+  end
+  return sum
 end
 
-print(sum)
+print(sumMuls(mem))
 
 -- 156388521
--- 2024/day-3/part2.lua
+++ 2024/day-3/part2.lua
@@ -1,15 +1,33 @@
 local U = require("util")
 
 local mem = U.readFile("./input.txt")
-local sum = 0
 
-for mulStr in mem:gmatch("mul%(%d+,%d+%)") do
-  print(mulStr)
-  local product = 1
-  for n in mulStr:gmatch("%d+") do
-    product = product * n
+local total = 0
+local current = 1
+local nextBadStart = mem:find("don't%(%)", current)
+
+while nextBadStart do
+  local stillLooking = true
+  while stillLooking do
+    local nextMulStart, _, x, y = mem:find("mul%((%d+),(%d+)%)", current)
+    if nextMulStart == nil then -- if there are no more muls, just call it a day no matter what
+      stillLooking = false
+      current = #mem
+    elseif nextMulStart > nextBadStart then -- if the next mul is in bad-town, ignore it and move us back up to good town
+      local nextGoodTimes, _ = mem:find("do%(%)", nextBadStart)
+      current = nextGoodTimes or #mem -- if there are no more good times to be had, skip to the end
+      stillLooking = false -- time to find the next bad
+    else
+      -- found a mul and it's before the next don't()? Tally it up
+      current = nextMulStart + 5
+      total = total + (x * y)
+    end
   end
-  sum = sum + product
+  nextBadStart = mem:find("don't%(%)", current)
 end
 
-print(sum)
+print(total)
+
+-- 2814993378 (too high)
+-- 75920122 (correct) (kinda cheated though by just using vim and macros and stuff to clean up the input)
+-- 75920122 (got it again but straight from raw input. yay?)
